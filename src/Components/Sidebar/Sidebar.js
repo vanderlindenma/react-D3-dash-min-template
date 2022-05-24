70849f78ed59f3bfa8c873985b426c5087bbba75
vanderlindenma <vanderlindenma@users.noreply.github.com>
2022-05-24 10:05:55 +0200

    Improve UI example flexibility passing atom as prop at lowest level

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import {Heading} from 'react-bulma-components';
 import TFCheckbox from 'Components/UI/TFCheckbox'; // Absolute path import from src enabled
 import './Sidebar.css'; // Although absolute path import from src enabled, can also import using relative path
+import { exampleControlState } from 'jotaiStore';
 
 const Sidebar = () => {
+
     return (
         <div id = 'Sidebar'>
             <Heading> Sidebar </Heading>
-            <TFCheckbox/>
+            <TFCheckbox atom = {exampleControlState}/>
             Example control
         </div>
     )
